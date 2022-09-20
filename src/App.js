009b38453f0ee1341ed57f0b34921e96ad5a3571
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 22:53:11 +0600

    react-recp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+      <LoadCountries></LoadCountries>
+      
     </div>
   );
+}
+
+function LoadCountries() {
+  const [counties, setCounties] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCounties(data))
+  }, []);
+  return (
+    <div>
+      <h2>counties tour yey...</h2>
+      {/* <h3>Countries: { counties.length}</h3> */}
+      {
+        counties.map(country => <Country name={country.name.common}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: { props.name}</h2>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseCounter = () => (setCount(count + 1));
+  const decreaseCounter = () => (setCount(count -1));
+  return (
+    <div className='counter'>
+      <h2>Count { count}</h2>
+      <div className='btn-count'>
+        <button className='btn' onClick={increaseCounter}>Increase</button>
+        <button className='btn' onClick={decreaseCounter}>Decrease</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
