7ac0f5cb97c2ddf76ccf4a9df9d6fde1b6fab93f
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-20 23:56:44 +0600

    doe

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
-      <LoadCountries></LoadCountries>
-      
+      <Countries></Countries>
+      <Header></Header>
+      <Person></Person>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [counties, setCounties] = useState([])
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCounties(data))
-  }, []);
-  return (
-    <div>
-      <h2>counties tour yey...</h2>
-      {/* <h3>Countries: { counties.length}</h3> */}
-      {
-        counties.map(country => <Country name={country.name.common}></Country>)
-      }
-    </div>
-  )
-}
+// function LoadCountries() {
+//   const [countries, setCounties] = useState([])
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCounties(data))
+//   }, []);
+//   return (
+//     <div>
+//       <h2>counties tour yey...</h2>
+//       {/* <h3>Countries: { counties.length}</h3> */}
+//       {
+//         countries.map(country => <Country name={country.name.common}></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props) {
-  return (
-    <div>
-      <h2>Name: { props.name}</h2>
-    </div>
-  )
-}
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name: { props.name}</h2>
+//     </div>
+//   )
+// }
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const increaseCounter = () => (setCount(count + 1));
-  const decreaseCounter = () => (setCount(count -1));
-  return (
-    <div className='counter'>
-      <h2>Count { count}</h2>
-      <div className='btn-count'>
-        <button className='btn' onClick={increaseCounter}>Increase</button>
-        <button className='btn' onClick={decreaseCounter}>Decrease</button>
-      </div>
-    </div>
-  )
-}
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   const increaseCounter = () => (setCount(count + 1));
+//   const decreaseCounter = () => (setCount(count -1));
+//   return (
+//     <div className='counter'>
+//       <h2>Count { count}</h2>
+//       <div className='btn-count'>
+//         <button className='btn' onClick={increaseCounter}>Increase</button>
+//         <button className='btn' onClick={decreaseCounter}>Decrease</button>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+    return (
+        <div>
+            <h1>Welcome Person</h1>
+        </div>
+    );
+};
+
+export default Person;
